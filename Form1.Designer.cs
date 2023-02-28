27a869e91eb147e034b98d1553887a1657ef2fe7
arudzik <arudzik@INFORM-PC-52>
2023-02-28 07:50:24 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace MySQLDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 14.25F);
+            this.textBox1.Location = new System.Drawing.Point(1, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(440, 169);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(447, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 72);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Execute Query";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-2, 345);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Oddziały";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1, 193);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(440, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "ID";
+            this.textBox2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox2_MouseClick);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1, 219);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(440, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "Nazwa";
+            this.textBox3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox3_MouseClick);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1, 271);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(440, 20);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "id_gn";
+            this.textBox4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox4_MouseClick);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(1, 245);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(440, 20);
+            this.textBox5.TabIndex = 6;
+            this.textBox5.Text = "opk";
+            this.textBox5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox5_MouseClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label2.Location = new System.Drawing.Point(150, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Dodaj Oddział";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.button2.Location = new System.Drawing.Point(366, 297);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 48);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 357);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using MySql.Data.MySqlClient;
+
+namespace MySQLDatabase
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string MySqlConnectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=mydb";
+ 
+        public void button1_Click(object sender, EventArgs e)
+        {
+            runQuery();
+        }
+        public void runQuery()
+        {
+            string query = textBox1.Text;
+            if(query == "")
+            {
+                MessageBox.Show("Please insert some sql querry!");
+                    return;
+            }
+
+
+            MySqlConnection databaseConnection = new MySqlConnection(MySqlConnectionString);
+
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+
+            try
+            {
+                databaseConnection.Open();
+                MySqlDataReader myReader = commandDatabase.ExecuteReader();
+                if (myReader.HasRows)
+                {
+                    MessageBox.Show("Your query generated results, please see the console");
+
+                    while (myReader.Read())
+                    {
+                        Console.WriteLine(myReader.GetString(0) + " - " + myReader.GetString(1) + " - " + myReader.GetString(2) + " - " + myReader.GetString(3));
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Query successfull executed");
+
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Query error " + e.Message);
+
+            }
+
+        }
+
+       public void label1_Click(object sender, EventArgs e)
+        {
+            string customQuery = "SELECT * FROM `oddzialy`";
+            MySqlConnection databaseConnection = new MySqlConnection(MySqlConnectionString);
+            MySqlCommand questDb = new MySqlCommand(customQuery, databaseConnection);
+            databaseConnection.Open();
+            MySqlDataReader reader = questDb.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(0) + " - " + reader.GetString(1) + " - " + reader.GetString(2) + " - " + reader.GetString(3));
+            }
+
+        }
+
+        private void textBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox2.Text = string.Empty;
+
+        }
+
+        private void textBox3_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox3.Text = string.Empty;
+
+        }
+
+        private void textBox5_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox5.Text = string.Empty;
+
+        }
+
+        private void textBox4_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox4.Text = string.Empty;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string id = textBox2.Text;
+            string odd = textBox3.Text;
+            string opk = textBox5.Text;
+            string idG = textBox4.Text;
+            string customQuery = "INSERT INTO oddzialy(id, oddzial, opk_5, id_gn) VALUES ('" + id + "', '" + odd + "', '" + opk + "', '" + idG + "')";
+            try
+            {
+                if (string.IsNullOrEmpty(id)) {
+                    MessageBox.Show("Id nie może być puste");
+                        return;
+                }
+                MySqlConnection databaseConnection = new MySqlConnection(MySqlConnectionString);
+                MySqlCommand quest = new MySqlCommand(customQuery, databaseConnection);
+                databaseConnection.Open();
+                MySqlDataReader inputData = quest.ExecuteReader();
+
+            }catch(Exception ex) { MessageBox.Show("Błąd: " + ex.Message); }
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+        //public void textBox2_Text(object sender, EventArgs e)
+        //{
+
+        //    TextBox mytextbox = new TextBox();
+        //    databaseConnection.Open();
+        //        string customQuery = "SELECT * FROM `oddzialy`";
+        //    MySqlCommand zapytanie = new MySqlCommand(customQuery, databaseConnection);
+        //    MySqlDataReader czytacz = zapytanie.ExecuteReader();
+        //    Console.WriteLine
+        //}
+    }
+}
